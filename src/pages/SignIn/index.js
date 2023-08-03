2dd651397da04847bb4a632c9eb937418ab8f561
Gil Carvalhal <gilbertocarvalhalmatos@gmail.com>
2023-08-02 21:14:49 -0300

    criando pagina login

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,10 +1,35 @@
+import { useState } from 'react';
+import './signin.css';
+import logo from '../../assets/logo.png'
+
+import { Link } from 'react-router-dom';
 
 export default function SignIn() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+
     return (
-        <div>
-            <h1>
-                Página login
-            </h1>
+        <div className='container-center'>
+            <div className='login'>
+                <div className='login-area'>
+                    <img src={logo} alt='Logo do sistema de chamados' />
+                </div>
+
+                <form>
+                    <h1>
+                        Entrar
+                    </h1>
+                    <input type='text' placeholder='email@email.com' value={email} onChange={(e) => setEmail(e.target.value)} />
+                    <input type='password' placeholder='*******' value={password} onChange={(e) => setPassword(e.target.value)} />
+
+                    <button type='submit'>Acessar</button>
+                </form>
+
+                <Link to="/register">
+                    Criar uma conta
+                </Link>
+            </div>
         </div>
     )
 }
