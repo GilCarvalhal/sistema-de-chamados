890651bd6a906dec18d504ecb5e6463a5deac0ce
Gil Carvalhal <gilbertocarvalhalmatos@gmail.com>
2023-08-03 21:05:45 -0300

    cadastrando usuario pt2

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -3,11 +3,16 @@ import { auth, db } from '../services/firebaseConnection';
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { doc, getDoc, setDoc } from "firebase/firestore";
 
+import { useNavigate } from "react-router-dom";
+import { toast } from 'react-toastify';
+
 export const AuthContext = createContext({});
 
 function AuthProvider({ children }) {
     const [user, setUSer] = useState(null);
     const [loadingAuth, setLoadingAuth] = useState(false);
+
+    const navigate = useNavigate();
 
     function signIn(email, password) {
         console.log(email);
@@ -35,14 +40,20 @@ function AuthProvider({ children }) {
                             avatarUrl: null,
                         };
                         setUSer(data);
-
+                        storageUser(data);
                         setLoadingAuth(false);
+                        toast.success('Seja bem-vindo ao sistema!');
+                        navigate("/dashboard");
                     })
             })
             .catch((error) => {
                 console.log(error);
                 setLoadingAuth(false);
             })
+    }
+
+    function storageUser(data) {
+        localStorage.setItem('@tickets', JSON.stringify(data))
     }
 
     return (
