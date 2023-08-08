77679579857ad6025eca8f03f0f9e0e778f203eb
Gil Carvalhal <gilbertocarvalhalmatos@gmail.com>
2023-08-08 12:47:01 -0300

    criando pagina dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -2,6 +2,11 @@ import { useContext } from "react";
 import { AuthContext } from "../../contexts/auth";
 
 import Header from "../../components/Header";
+import Title from '../../components/Title'
+import { FiPlus, FiMessageSquare, FiSearch, FiEdit2 } from "react-icons/fi";
+import { Link } from "react-router-dom";
+
+import './dashboard.css'
 
 export default function Dashboard() {
     const { logout } = useContext(AuthContext);
@@ -13,10 +18,67 @@ export default function Dashboard() {
     return (
         <div>
             <Header />
-            <h1>
-                Página dashboard
-            </h1>
-            <button onClick={handleLogout}>Sair da conta</button>
+
+            <div className="content">
+                <Title name="Tickets">
+                    <FiMessageSquare size={25} />
+                </Title>
+
+                <>
+                    <Link to='/new' className="new">
+                        <FiPlus size={25} color="#fff" />
+                        Novo chamado
+                    </Link>
+
+                    <table>
+                        <thead>
+                            <tr>
+                                <th scope="col">
+                                    Cliente
+                                </th>
+                                <th scope="col">
+                                    Assunto
+                                </th>
+                                <th scope="col">
+                                    Status
+                                </th>
+                                <th scope="col">
+                                    Cadastrado em
+                                </th>
+                                <th scope="col">
+                                    #
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td data-label="Cliente">
+                                    Mercado esquina
+                                </td>
+                                <td data-label="Assunto">
+                                    Suporte
+                                </td>
+                                <td data-label="Status">
+                                    Em aberto
+                                </td>
+                                <td data-label="Cadastrado">
+                                    08/08/2023
+                                </td>
+                                <td data-label="#">
+                                    <button className="action" style={{ backgroundColor: '#3583f6' }}>
+                                        <FiSearch size={17} color="#fffF" />
+                                    </button>
+                                    <button className="action" style={{ backgroundColor: '#f6a935' }}>
+                                        <FiEdit2 size={17} color="#fff" />
+                                    </button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+                </>
+            </div>
+
         </div>
     )
 }
